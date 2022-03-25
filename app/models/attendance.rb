8199698b8268002632fa43397e10d7ba33f5ffeb
akk962 <akk962@tamu.edu>
2022-03-25 08:20:56 -0500

    Updated search bars for full name functionality

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -2,4 +2,8 @@ class Attendance < ApplicationRecord
     belongs_to :event 
     belongs_to :member 
     validates :attended, :rsvp, inclusion: [true, false]
+
+    ransacker :full_name do
+        Arel.sql("CONCAT_WS(' | ', members.fname, members.lname)")
+      end
 end
