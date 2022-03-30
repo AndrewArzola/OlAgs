9579df1e52e39e4178e1e269af9977390dedec32
writchie3 <writchieiii@gmail.com>
2022-03-30 00:40:56 +0000

    added warning ignore for brakeman due to requirements from customer

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -75,6 +75,6 @@ class MembersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def member_params
-    params.require(:member).permit(:fname, :lname, :joinDate, :gradDate, :email, :phoneNumber, :city, :admin, :major, :active)
+    params.require(:member).permit(:fname, :lname, :joinDate, :gradDate, :email, :phoneNumber, :city, :major, :active, :admin)
   end
 end
