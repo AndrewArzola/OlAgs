09434868c2847d2062f92c2cc8a1b85b513048e7
AndrewArzola <andrew.j.arzola@gmail.com>
2022-03-30 19:37:58 -0500

    implement lineage
    Lineage prints wrong and html is not implemented

--- app/models/lineage.rb
+++ app/models/lineage.rb
@@ -3,9 +3,6 @@ include ActionView::Helpers::UrlHelper
 class Lineage < ApplicationRecord
   belongs_to :member
   validates :member_id, uniqueness: { scope: :member_id }
-  validates :father, uniqueness: { scope: :father }
-  validates :son, uniqueness: { scope: :son }
-  validate :check_equality
   validate :at_least_two_nodes
 
   def check_equality
