8f76b441d501b052f704121a6ab5103019d5cf96
AndrewArzola <andrew.j.arzola@gmail.com>
2022-04-04 18:12:27 -0500

    run rubocop
    fixed rubocop

--- app/models/lineage.rb
+++ app/models/lineage.rb
@@ -5,16 +5,17 @@ class Lineage < ApplicationRecord
   belongs_to :member
   # rubocop:disable Rails/UniqueValidationWithoutIndex
   validates :member_id, uniqueness: { scope: :member_id }
-  validates :father, uniqueness: { scope: :father }, unless: Proc.new { |b| b.father.blank? }
-  validates :son, uniqueness: { scope: :son }, unless: Proc.new { |b| b.son.blank? }
+  validates :father, uniqueness: { scope: :father }, unless: proc { |b| b.father.blank? }
+  validates :son, uniqueness: { scope: :son }, unless: proc { |b| b.son.blank? }
   validate :check_equality
   # rubocop:enable Rails/UniqueValidationWithoutIndex
 
   def check_equality
-    errors.add(:son, ": Big and Little can't be the same") if father == son && (!son.blank? && !father.blank?)
+    errors.add(:son, ": Big and Little can't be the same") if father == son && (son.present? && father.present?)
     errors.add(:member_id, ": Little can't be the same as member id") if son == member_id
     errors.add(:member_id, ": Big can't be the same as member id") if father == member_id
   end
+
   def name_check(attribute)
     if Member.exists?(id: attribute)
       link_to(Member.where(id: attribute).last.fname, Rails.application.routes.url_helpers.member_path(attribute))
