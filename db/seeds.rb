fad7ddcb1bbf52d0d450309cc7d00b0ee90735be
AndrewArzola <andrew.j.arzola@gmail.com>
2022-04-03 20:23:16 -0500

    Edit lineage and seed
    Added admin panel in lineage
    Added joindates in seed
    Added links to lineages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 if Member.count == 0
   Member.create!([
     {fname: "Walter", lname: "Ritchie", joinDate: "2022-03-04", email: "writchie3@tamu.edu", phoneNumber: "2148936415", city: "College Station", admin: true, major: "Computer Science", active: true, gradDate: "2022-03-04"},
-    {fname: "CJ", lname: "Sewell", joinDate: nil, email: "c.j.sewell07@tamu.edu", phoneNumber: "", city: "", admin: true, major: "Computer Engineering", active: true, gradDate: nil},
-    {fname: "Anna", lname: "Kolodziejcyk", joinDate: nil, email: "akk962@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: true, gradDate: nil},
-    {fname: "Alex", lname: "Torres", joinDate: nil, email: "robincrass@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: false, gradDate: nil},
-    {fname: "Andrew", lname: "Arzola", joinDate: nil, email: "andrew.j.arzola@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: false, gradDate: nil}
+    {fname: "CJ", lname: "Sewell", joinDate: "2021-03-04", email: "c.j.sewell07@tamu.edu", phoneNumber: "", city: "", admin: true, major: "Computer Engineering", active: true, gradDate: nil},
+    {fname: "Anna", lname: "Kolodziejcyk", joinDate: "2020-03-04", email: "akk962@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: true, gradDate: nil},
+    {fname: "Alex", lname: "Torres", joinDate: "2019-03-04", email: "robincrass@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: false, gradDate: nil},
+    {fname: "Andrew", lname: "Arzola", joinDate: "2018-03-04", email: "andrew.j.arzola@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: false, gradDate: nil}
   ])
 end
